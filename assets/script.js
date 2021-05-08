2874522ef54b5d1a03207b6930a338af14ba7c4e
Stanley <aynbunda@gmail.com>
2021-05-08 16:59:24 -0400

    starter code

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: [alerts]
+    }
+]
